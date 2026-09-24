2b81acd4e4f4f759f82f5c509e52ae4fead9006b
Bella Smith <smithic@rose-hulman.edu>
2026-09-24 13:47:21 -0400

    pyserial tutorial

--- Lab 3/initial_pyserial_tutorial.py
+++ Lab 3/initial_pyserial_tutorial.py
@@ -1,23 +1,24 @@
 import serial
+import time
 
 print("Learning Pyserial")
 
 #below, when the port is set... refer to the arduino with /dev/ttyACM0. refer to plateloader with /dev/ttyUSB0
 ser = serial.Serial(port="/dev/ttyACM0", baudrate=19200, timeout=10) #check the com number !!! # create the object
 # may or may not need ser.open()
-while not ser.is_open:
-    
-    time.sleep(2.0) # Necessary sometimes
-    ser.reset_input_buffer() # clears off old responses
-    message = "RESET" # have to convert string into byte array
-    message_bytes = (message + "\n").encode()
-    print(message_bytes)
 
-    ser.write(message_bytes)
+time.sleep(2.0) # Necessary sometimes
+ser.reset_input_buffer() # clears off old responses
+message = "RESET" # have to convert string into byte array
+print(message)
+message_bytes = (message + "\n").encode()
+print(message_bytes)
 
-    response_bytes = ser.readline()
-    print(response_bytes)
-    response = response_bytes.decode().strip()
-    print(response)
+ser.write(message_bytes)
+
+response_bytes = ser.readline()
+print(response_bytes)
+response = response_bytes.decode().strip()
+print(response)
 
 ser.close() # what does opening and closing the serial object mean?
